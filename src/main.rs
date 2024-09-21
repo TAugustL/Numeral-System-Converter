ec3d839e3e07b9d9f30ea17bc0b5edba2b28e49e
August <138250131+TAugustL@users.noreply.github.com>
2024-09-21 20:48:39 +0200

    major overhaul
    you can now convert numbers from bases 2 up to 32, also UI is improved

--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,112 @@
-use std::io;
+use std::io::{self, Write};
 
-struct Conversion {
-    start_type: String,
-    start_value: String,
+struct Conversion<'a> {
+    value: String,
+    start_base: u32,
+    target_base: Vec<&'a str>,
 }
 
-impl Conversion {
-    fn calc_target_value(&self) {
-        if self.start_type == "dec" {
-            let decimal: u128 = u128::from_str_radix(&self.start_value, 10).unwrap();
+impl Conversion<'_> {
+    fn convert(&self) -> Vec<String> {
+        let mut numbers: Vec<String> = Vec::new();
 
-            let dec_line: usize = format!("|Results for {}: |", decimal).len();
-            let hex_line: usize = format!("|Hex number: 0x{:x} |", decimal).len();
-            let bin_line: usize = format!("|Bin number: 0b{:b} |", decimal).len();
-            let oct_line: usize = format!("|Oct number: 0o{:o} |", decimal).len();
+        for (i, base) in self.target_base.clone().into_iter().enumerate() {
+            if [0, 1].contains(&i) {
+                continue;
+            }
 
-            println!("\n{}", "—".repeat(bin_line));
-            println!("|Results for {}:{} |", self.start_value, " ".repeat(bin_line - dec_line));
-            println!("|{}|", " ".repeat(bin_line - 2));
-            println!("|Hex number: {}{} |", format!("0x{:x}", decimal), " ".repeat(bin_line - hex_line));
-            println!("|Bin number: {} |", format!("0b{:b}", decimal));
-            println!("|Oct number: {}{} |", format!("0o{:o}", decimal), " ".repeat(bin_line - oct_line));
-            println!("{}", "—".repeat(bin_line));
+            let base: u32 = base.parse().expect("invalid base!");
+            let decimal: u32 =
+                u32::from_str_radix(&self.value, self.start_base).expect("invalid number!");
+            let mut number: String = String::new();
 
-        } else if self.start_type == "bin" {
-            let decimal: u128 = u128::from_str_radix(&self.start_value, 2).unwrap();
+            let remainder = decimal % base;
+            let mut quotient = decimal / base;
+            let mut a = String::new();
+            a.push_str(&format!("{remainder}{number}"));
+            number = a;
 
-            let dec_line: usize = format!("|Dec number: {} |", decimal).len();
-            let hex_line: usize = format!("|Hex number: 0x{:x} |", decimal).len();
-            let bin_line: usize = format!("|Results for 0b{:b}: |", decimal).len();
-            let oct_line: usize = format!("|Oct number: 0o{:o} |", decimal).len();
+            while quotient != 0 {
+                let remainder = quotient % base;
+                quotient /= base;
 
-            println!("\n{}", "—".repeat(bin_line));
-            println!("|Results for 0b{}: |", self.start_value);
-            println!("|{}|", " ".repeat(bin_line - 2));
-            println!("|Hex number: {}{} |", format!("0x{:x}", decimal), " ".repeat(bin_line - hex_line));
-            println!("|Dec number: {}{} |", decimal, " ".repeat(bin_line - dec_line));
-            println!("|Oct number: {}{} |", format!("0o{:o}", decimal), " ".repeat(bin_line - oct_line));
-            println!("{}", "—".repeat(bin_line));
+                let mut b = String::new();
+                b.push_str(&format!("{remainder}{number}"));
 
-        } else if self.start_type == "hex" {
-            let decimal: u128 = u128::from_str_radix(&self.start_value, 16).unwrap();
-
-            let dec_line: usize = format!("|Dec number: {} |", decimal).len();
-            let hex_line: usize = format!("|Results for 0x{:x}: |", decimal).len();
-            let bin_line: usize = format!("|Bin number: 0b{:b} |", decimal).len();
-            let oct_line: usize = format!("|Oct number: 0o{:o} |", decimal).len();
-
-            println!("\n{}", "—".repeat(bin_line));
-            println!("|Results for 0x{}:{} |", self.start_value, " ".repeat(bin_line - hex_line));
-            println!("|{}|", " ".repeat(bin_line - 2));
-            println!("|Dec number: {}{} |", decimal, " ".repeat(bin_line - dec_line));
-            println!("|Bin number: {} |", format!("0b{:b}", decimal));
-            println!("|Oct number: {}{} |", format!("0o{:o}", decimal), " ".repeat(bin_line - oct_line));
-            println!("{}", "—".repeat(bin_line));
-
-        } else if self.start_type == "oct" {
-            let decimal: u128 = u128::from_str_radix(&self.start_value, 8).unwrap();
-
-            let dec_line: usize = format!("|Dec number: {} |", decimal).len();
-            let hex_line: usize = format!("|Hex number: 0x{:x} |", decimal).len();
-            let bin_line: usize = format!("|Bin number: 0b{:b} |", decimal).len();
-            let oct_line: usize = format!("|Results for 0o{:o}: |", decimal).len();
-
-            println!("\n{}", "—".repeat(bin_line));
-            println!("|Results for 0o{}:{} |", self.start_value, " ".repeat(bin_line - oct_line));
-            println!("|{}|", " ".repeat(bin_line - 2));
-            println!("|Hex number: {}{} |", format!("0x{:x}", decimal), " ".repeat(bin_line - hex_line));
-            println!("|Bin number: {} |", format!("0b{:b}", decimal));
-            println!("|Dec number: {}{} |",  decimal, " ".repeat(bin_line - dec_line));
-            println!("{}", "—".repeat(bin_line));
-
-        } else {
-            println!("Invalid input!");
+                number = b;
+            }
+            numbers.push(number)
         }
+        numbers
     }
 }
 
 fn main() {
-    println!("{}", "=".repeat(24));
-    println!("NUMERAL SYSTEM CONVERTER");
-    println!("{}", "=".repeat(24));
-    println!("Enter start number type:\n[dec] for decimal\n[hex] for hexadecimal\n[bin] for binary\n[oct] for octal\n");
+    let start_text: &str = "
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃ NUMERAL SYSTEM CONVERTER ┃
+┣━━━━━━━━━━━━━━━━━━━━━━━━━━┫
+┃ Enter \x1b[91mstart number\x1b[0m,      ┃
+┃ \x1b[92mstart base\x1b[0m and           ┃
+┃ \x1b[93mtarget base(s)\x1b[0m           ┃
+┃ (e.g. '\x1b[91m4 \x1b[92m6 \x1b[93m16 7\x1b[0m')        ┃
+┣━━━━━━━━━━━━━━━━━━━━━━━━━━┛";
+    println!("{start_text}");
+    print!("┃ ");
+    io::stdout().flush().unwrap();
 
-    let mut num_type: String = String::new();
-    io::stdin().read_line(&mut num_type).expect("Failed to read line!");
-    let num_type: String = num_type.trim().parse().expect("Failed to process input!");
+    let mut input: String = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let args: Vec<&str> = input.trim().split_whitespace().collect();
+    if args.len() < 3 {
+        println!("Too few arguments given!");
+        return;
+    }
+    let (value, start_base, target_base): (String, u32, Vec<&str>) = (
+        args[0].to_string(),
+        args[1].parse().expect("invalid start base!"),
+        args,
+    );
 
-    let start_type: String = String::from(num_type);
-
-    println!("\nEnter start value:\n");
-    let mut num_value: String = String::new();
-    io::stdin().read_line(&mut num_value).expect("Failed to read line!");
-    let num_value: String = num_value.trim().parse().expect("Failed to process input!");
-
-    let start_value: String = String::from(num_value);
-
-    let conversion = Conversion {
-        start_type,
-        start_value
+    let a: Conversion = Conversion {
+        value,
+        start_base,
+        target_base,
     };
 
-    conversion.calc_target_value();
+    let results = a.convert();
+
+    let mut max_length: usize = 31;
+
+    for (i, res) in results.iter().enumerate() {
+        let output: String = format!("┃ Base {}: {} ┃", a.target_base[i + 2], res);
+        if output.len() > max_length {
+            max_length = output.len();
+        }
+    }
+
+    println!("┣{}┓", "━".repeat(max_length - 5));
+    let output: String = format!("┃ {} from base {} to: ┃", a.value, a.start_base);
+    println!(
+        "┃ {} from base {} to: {} ┃",
+        a.value,
+        a.start_base,
+        " ".repeat(max_length - output.len())
+    );
+    println!("┣{}┫", "╍".repeat(max_length - 5));
+
+    for (i, res) in results.iter().enumerate() {
+        let output: String = format!("┃ base {}: {} ┃", a.target_base[i + 2], res);
+        let difference = max_length - output.len();
+        println!(
+            "┃ base {}: {} {} ┃",
+            a.target_base[i + 2],
+            res,
+            " ".repeat(difference)
+        );
+        if results[i] != *results.last().unwrap() {
+            println!("┠{}┨", "─".repeat(max_length - 5));
+        }
+    }
+    println!("┗{}┛\n", "━".repeat(max_length - 5));
 }
